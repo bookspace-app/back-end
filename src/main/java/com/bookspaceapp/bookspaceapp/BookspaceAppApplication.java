f2a18370e418bb6717e0b60e1a0b02dbdfa29e7a
Toni <garcia.calvin.toni@gmail.com>
2021-03-11 19:30:02 +0100

    First commit

---
+++ src/main/java/com/bookspaceapp/bookspaceapp/BookspaceAppApplication.java
@@ -0,0 +1,13 @@
+package com.bookspaceapp.bookspaceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookspaceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookspaceAppApplication.class, args);
+	}
+
+}
