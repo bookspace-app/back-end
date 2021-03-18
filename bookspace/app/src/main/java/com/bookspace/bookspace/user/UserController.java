2afa7e550ee645fe0d9f6f6acbb9dcad9e8b87fe
dekadri9 <adria.de.castro@estudiantat.upc.edu>
2021-03-18 10:19:54 +0100

    Main API func. for User class

--- bookspace/app/src/main/java/com/bookspace/bookspace/user/UserController.java
+++ bookspace/app/src/main/java/com/bookspace/bookspace/user/UserController.java
@@ -1,9 +1,12 @@
 package com.bookspace.bookspace.user;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,17 +24,29 @@ public class UserController {
         this.userService = userService;
     }
 
-    
-    @GetMapping    
-	public List<User> getStudent() {
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getUsers();
+    }
 
-        return userService.getStudents();
+    
+    @GetMapping(path = "{userId}")   
+	public Optional<User> getUserById(@PathVariable("userId") Long id) {
+        return userService.getUser(id);
     }
 
     @PostMapping
     public void registerNewUser(@RequestBody User user){
         userService.addNewUser(user);
     }
+
+    @DeleteMapping(path = "{userId}")
+    public void deleteUser(@PathVariable("userId") Long userId){
+        userService.deleteUser(userId);
+    }
+
+
+
     
     
 
--- bookspace/app/src/main/java/com/bookspace/bookspace/user/UserService.java
+++ bookspace/app/src/main/java/com/bookspace/bookspace/user/UserService.java
@@ -16,8 +16,14 @@ public class UserService {
 		this.userRepository = userRepository;
 	}
 
-    public List<User> getStudents() {
-        return userRepository.findAll();
+	public List<User> getUsers(){
+		return userRepository.findAll();
+	}
+
+    public Optional<User> getUser(Long id) {
+		boolean exists = userRepository.existsById(id);
+		if(!exists) throw new IllegalStateException("The user with id " + id + " does not exist");
+        return userRepository.findById(id);
     }
 
     public void addNewUser(User user) {
@@ -28,5 +34,14 @@ public class UserService {
 		}
 		userRepository.save(user);
     }
+
+	public void deleteUser(Long userId){
+		boolean b = userRepository.existsById(userId);
+		if(!b) {
+			throw new IllegalStateException("User with id " + userId + " does not exists");
+		}
+		userRepository.deleteById(userId);
+
+	}
     
 }
