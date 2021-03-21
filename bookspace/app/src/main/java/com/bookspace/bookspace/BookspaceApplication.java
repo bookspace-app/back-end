4344ac0f1aae8e6534c4b2b472c60019de43be0f
Toni <garcia.calvin.toni@gmail.com>
2021-03-21 18:17:23 +0100

    trying deployment

--- bookspace/app/src/main/java/com/bookspace/bookspace/BookspaceApplication.java
+++ bookspace/app/src/main/java/com/bookspace/bookspace/BookspaceApplication.java
@@ -2,15 +2,22 @@ package com.bookspace.bookspace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class BookspaceApplication {
 
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BookspaceApplication.class, args);
 	}
+}
 
+@RestController
+class HelloController {
+	@GetMapping("/")
+	String hello() {
+		return "hello world";
+	}
 }
