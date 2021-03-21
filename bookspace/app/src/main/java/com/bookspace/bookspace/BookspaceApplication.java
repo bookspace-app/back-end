43555d7a0d2606c6e05c806852b538c07c49bf14
Toni <garcia.calvin.toni@gmail.com>
2021-03-21 19:34:31 +0100

    deploying

--- bookspace/app/src/main/java/com/bookspace/bookspace/BookspaceApplication.java
+++ bookspace/app/src/main/java/com/bookspace/bookspace/BookspaceApplication.java
@@ -2,8 +2,6 @@ package com.bookspace.bookspace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class BookspaceApplication {
@@ -14,10 +12,3 @@ public class BookspaceApplication {
 	}
 }
 
-@RestController
-class HelloController {
-	@GetMapping("/")
-	String hello() {
-		return "hello world";
-	}
-}
