b9ded374df1431e85929e5788b3d5e9730c2ba56
julen.galera <julen.galera@estudiantat.upc.edu>
2021-05-06 09:48:49 +0200

    Tag comments

--- src/main/java/com/example/bookspace/models/Tag.java
+++ src/main/java/com/example/bookspace/models/Tag.java
@@ -13,9 +13,9 @@ import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
-
 @Entity
 @Table(name = "tag")
+//Tag model class
 public class Tag {
 
     @Id
@@ -44,59 +44,69 @@ public class Tag {
     @ManyToMany(mappedBy = "favTags")
     private List<User> users = new ArrayList<>();
 
-    
+    //Default constructor
     public Tag() {}
 
+    //Constructor given attributes --> {name, authorId, publication}
     public Tag (String name, User author, Publication publication) {
         this.name = name;
         this.author = author;
         this.publications.add(publication);
     }
 
+    //Constructor given attributes --> {name, authorId}
     public Tag (String name, User author) {
         this.name = name;
         this.author = author;
     }
     
-
+    //Getter of {id} attribute
     public Long getId() {
         return this.id;
     }
 
+    //Setter of {id} attribute
     public void setId(Long id) {
         this.id = id;
     }
 
+    //Getter of {name} attribute
     public String getName() {
         return this.name;
     }
 
+    //Setter of {name} attribute
     public void setName(String name) {
         this.name = name;
     }
 
+    //Getter of {author} attribute
     public User getAuthor() {
         return this.author;
     }
 
+    //Setter of {author} attribute
     public void setAuthor(User author) {
         this.author = author;
     }
 
+    //Getter of {publications} attribute
     public List<Publication> getPublications() {
         return this.publications;
     }
 
+    //Setter of {publications} attribute
     public void setPublications(List<Publication> publications) {
         this.publications = publications;
     }
 
+    //Getter of {users} attribute
     public List<User> getUsers() {
         return this.users;
     }
 
+    //Setter of {users} attribute
     public void setUsers(List<User> users) {
         this.users = users;
     }
-
 }
--- src/main/java/com/example/bookspace/repositories/TagRepository.java
+++ src/main/java/com/example/bookspace/repositories/TagRepository.java
@@ -7,8 +7,10 @@ import com.example.bookspace.models.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
+//Repository class for Tag
 public interface TagRepository extends JpaRepository<Tag, Long> {
+
+    //It finds the Tag in the DB associated with the given attribute {name}
     Optional<Tag> findTagByName(String name);
 }
