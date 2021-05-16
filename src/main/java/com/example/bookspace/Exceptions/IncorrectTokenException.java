083553037f46c887ba73f054321c84096aa2b9f9
Toni <garcia.calvin.toni@gmail.com>
2021-05-16 18:49:00 +0200

    Publication endpoints with token + some custom exceptions

--- src/main/java/com/example/bookspace/Exceptions/IncorrectTokenException.java
+++ src/main/java/com/example/bookspace/Exceptions/IncorrectTokenException.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "The token is incorrect")
-public class IncorrectTokenException extends Exception{
+public class IncorrectTokenException extends RuntimeException{
 
     public IncorrectTokenException() {}
     
--- src/main/java/com/example/bookspace/Exceptions/UserNotFoundException.java
+++ src/main/java/com/example/bookspace/Exceptions/UserNotFoundException.java
@@ -3,8 +3,8 @@ package com.example.bookspace.Exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "The user is not found")
-public class UserNotFoundException extends Exception {
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
 
     public UserNotFoundException(Long id) {
         super("The user with id " + id + " does not exists!");
