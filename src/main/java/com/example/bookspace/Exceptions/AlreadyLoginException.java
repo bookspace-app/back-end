bc8594a46675a2289e041ee19ceb049f85a0dac5
Toni <garcia.calvin.toni@gmail.com>
2021-05-20 20:15:57 +0200

    New Exception and merge resolved

---
+++ src/main/java/com/example/bookspace/Exceptions/AlreadyLoginException.java
@@ -0,0 +1,10 @@
+package com.example.bookspace.Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT, reason = "The user is already logged in")
+
+public class AlreadyLoginException extends RuntimeException {
+    
+}
