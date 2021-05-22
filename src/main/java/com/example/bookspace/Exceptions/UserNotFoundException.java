1e2625be04051a158465be0e3ffc82803adcfe21
Toni <garcia.calvin.toni@gmail.com>
2021-05-22 23:00:16 +0200

    trying things

--- src/main/java/com/example/bookspace/Exceptions/UserNotFoundException.java
+++ src/main/java/com/example/bookspace/Exceptions/UserNotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class UserNotFoundException extends RuntimeException {
+public class UserNotFoundException extends Exception {
 
     public UserNotFoundException(Long id) {
         super("The user with id " + id + " does not exists!");
