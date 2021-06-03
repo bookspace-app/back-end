ce6d830905b2e1b9dd9c24265433518efad52e34
dekadri9 <adria.de.castro@estudiantat.upc.edu>
2021-06-03 16:35:13 +0200

    Minor bug fixes for quality

--- src/main/java/com/example/bookspace/services/PublicationService.java
+++ src/main/java/com/example/bookspace/services/PublicationService.java
@@ -30,9 +30,7 @@ import com.example.bookspace.repositories.PublicationRepository;
 import com.example.bookspace.repositories.TagRepository;
 import com.example.bookspace.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 
 @Service
 public class PublicationService {
